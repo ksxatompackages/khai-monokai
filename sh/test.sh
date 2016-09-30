2c1023d86aed4d173f5861fac2804bf9ad9639a9
khai96_ <hvksmr1996@gmail.com>
2016-09-30 13:19:26 +0700

    Update tester

--- sh/test.sh
+++ sh/test.sh
@@ -8,4 +8,13 @@
     cat stdout.tmp
     exit 2
   )
+) && (
+  printf "Compiling LESS files... "
+  lessc ./index.less ./temp/index.css > stdout.tmp 2> stderr.tmp && (
+    echo "passed"
+  ) || (
+    echo "failed" >&2
+    cat stderr.tmp >&2
+    cat stdout.tmp
+  )
 )
